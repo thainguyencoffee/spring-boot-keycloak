84f26d1c0da7285129a4e8e2e7b4809396bcc20c
thainguyencoffee <nguyennt11032004@gmail.com>
2024-12-09 20:32:21 +0700

    add spring mvc oauth2 client

---
+++ spring-boot-mvc-client/src/main/java/com/example/springbootmvcclient/SpringBootMvcClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcClientApplication.class, args);
+    }
+
+}
---
+++ spring-boot-mvc-client/src/test/java/com/example/springbootmvcclient/SpringBootMvcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
