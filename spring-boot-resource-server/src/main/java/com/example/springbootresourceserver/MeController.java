94ec8d6f6130179296ce347f5eb3c0f197f2bc27
thainguyencoffee <nguyennt11032004@gmail.com>
2024-12-09 21:52:54 +0700

    add spring resource server

---
+++ spring-boot-resource-server/src/main/java/com/example/springbootresourceserver/MeController.java
@@ -0,0 +1,24 @@
+package com.example.springbootresourceserver;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MeController {
+
+    @GetMapping("/me")
+    public UserInfoDto me(JwtAuthenticationToken auth) {
+        return new UserInfoDto(
+                auth.getToken().getClaimAsString(StandardClaimNames.PREFERRED_USERNAME),
+                auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList());
+    }
+
+    public record UserInfoDto(String name, List<String> roles) {
+    }
+
+}
---
+++ spring-boot-resource-server/src/test/java/com/example/springbootresourceserver/SpringBootResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
